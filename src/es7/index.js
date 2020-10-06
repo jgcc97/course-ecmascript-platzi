68ed5668b3c2e7b542494bdc1a25ba901eab79cd
Jose Cafaro <gabrielcafaro@gmail.com>
2020-10-06 13:00:24 -0400

    Fifth class, Includes and Pow

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+/* INCLUDES STRUCTURE */
+let numbers = [1,2,3,6,7,2,8];
+
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7');
+}
+else {
+    console.log('No se encuentra el valor 7');
+}
+
+/* POW STRUCTURE */
+let base = 4;
+let exponent = 4;
+
+let result = base ** exponent;
+
+console.log(result);
